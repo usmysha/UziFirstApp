298d173dc2dec685ffb455f482d98cc7fd759833
shoaibd <shoaib.mohammed@hmhco.com>
2018-09-28 20:26:24 -0500

    Add project files.

---
+++ UziFirstApp/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace UziFirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MyFirstButon = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Message = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MyFirstButon
+            // 
+            this.MyFirstButon.Location = new System.Drawing.Point(34, 26);
+            this.MyFirstButon.Name = "MyFirstButon";
+            this.MyFirstButon.Size = new System.Drawing.Size(201, 23);
+            this.MyFirstButon.TabIndex = 0;
+            this.MyFirstButon.Text = "My First Button";
+            this.MyFirstButon.UseVisualStyleBackColor = true;
+            this.MyFirstButon.Click += new System.EventHandler(this.MyFirstButon_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(415, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(415, 206);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(325, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(415, 273);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(325, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(412, 246);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(53, 13);
+            this.Message.TabIndex = 4;
+            this.Message.Text = "Message:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Variable B";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(356, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Variable A";
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(90, 120);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(162, 23);
+            this.plus.TabIndex = 8;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(90, 173);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(162, 23);
+            this.minus.TabIndex = 9;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(90, 222);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(162, 23);
+            this.multiply.TabIndex = 10;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // division
+            // 
+            this.division.Location = new System.Drawing.Point(93, 276);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(159, 23);
+            this.division.TabIndex = 11;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.division_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.MyFirstButon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MyFirstButon;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button division;
+    }
+}
+
---
+++ UziFirstApp/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UziFirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MyFirstButon_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                MyDataTypes();
+                MyTypeconversion();
+
+
+                textBox3.Text = (Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)).ToString();
+            }
+            catch (Exception ex)
+            {
+                Message.Text = ex.Message;
+
+            }
+
+        }
+
+
+        public void MyDataTypes()
+        {
+            //https://www.tutorialspoint.com/csharp/csharp_data_types.htm
+            string someVarible = "This is My first Program 123";
+            int someVarible2 = 1;
+            bool someVarible4 = true;    //false
+            double someVarible3 = 1.1;
+        }
+
+        public void MyTypeconversion()
+        {
+            //https://www.tutorialspoint.com/csharp/csharp_data_types.htm
+            string someVarible1 = "123";
+            int someVarible2 = 52;
+
+            bool someVarible4 = true;    //false
+            double someVarible3 = 1.1;
+
+            try
+            {
+                int uziTypeCov1 = Convert.ToInt32(someVarible1);
+                string uziTypeCov2 = Convert.ToString(someVarible2);
+                string uziTypeCov3 = someVarible2.ToString();
+            }
+            catch (Exception ex)
+            {
+                //label1.Text = ex.Message; ;
+            }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            //textBox3.Text = (Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)).ToString();
+            string valueFromTextBoxA = textBox1.Text;
+            string valueFromTextBoxB = textBox2.Text;
+
+            string valueFromTextBoxC = textBox3.Text;
+
+            int x = Convert.ToInt32(valueFromTextBoxA);
+            int y = Convert.ToInt32(valueFromTextBoxB);
+
+            int r = x + y;
+
+            textBox3.Text = r.ToString();
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            string valueFromTextBoxA = textBox1.Text;
+            string valueFromTextBoxB = textBox2.Text;
+
+            string valueFromTextBoxC = textBox3.Text;
+
+            int x = Convert.ToInt32(valueFromTextBoxA);
+            int y = Convert.ToInt32(valueFromTextBoxB);
+
+            int r = x - y;
+
+            textBox3.Text = r.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            string valueFromTextBoxA = textBox1.Text;
+            string valueFromTextBoxB = textBox2.Text;
+
+            //string valueFromTextBoxC = textBox3.Text;
+
+            int x = Convert.ToInt32(valueFromTextBoxA);
+            int y = Convert.ToInt32(valueFromTextBoxB);
+
+            int r = x * y;
+
+            textBox3.Text = r.ToString();
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            string valuefromtextboxu = textBox1.Text;
+            string valuefromtextboxs = textBox2.Text;
+           // string valuefromtextboxm = textBox3.Text;
+
+            int p = Convert.ToInt32(valuefromtextboxu);
+            int q = Convert.ToInt32(valuefromtextboxs);
+            int r = p / q;
+
+            textBox3.Text = r.ToString();
+
+            
+        }
+    }
+}
+
